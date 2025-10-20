7b0ace1a7015eec92ff4e182e5db3bddb6f36884
ANON <spunkburger3@gmail.com>
2025-10-20 18:39:10 +0200

    rendering van grid werkt, comments toegevoegd

--- src/app/models/grid.model.ts
+++ src/app/models/grid.model.ts
@@ -1,2 +1,22 @@
+import * as THREE from 'three';
+
 export class Grid {
+    //variabelen
+    geometry!: THREE.BoxGeometry;
+    material!: THREE.MeshBasicMaterial;
+    grid!: THREE.GridHelper;
+    mesh!: THREE.Mesh;
+    gridSize!: number;
+    gridDivisions!: number;
+
+    //grid initialiseren
+    initialize(): void {
+        this.geometry = new THREE.BoxGeometry(30,0.1,30);
+        this.material = new THREE.MeshBasicMaterial({color: 0x000000});
+        this.mesh = new THREE.Mesh (this.geometry, this.material);
+        this.gridSize = 30;
+        this.gridDivisions = 30;
+        this.grid = new THREE.GridHelper(this.gridSize, this.gridDivisions, 0xffffff, 0xffffff);
+        this.grid.position.y = 0.1;
+    }
 }
--- src/app/pages/simulation/simulation.ts
+++ src/app/pages/simulation/simulation.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import * as THREE from 'three';
+import { Grid } from '../../models/grid.model';
 
 @Component({
   selector: 'app-simulation',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './simulation.html',
   styleUrl: './simulation.css'
 })
-export class Simulation {
+//afterviewinit voor width en height te kunnen uitlezen, geprobeerd met OnInit, maar dan kloppen de waarden niet
+export class Simulation implements AfterViewInit{
+  //viewchild voor reference naar DOM element in plaats van values uit te lezen, static : true zodat playfield beschikbaar is oninit, elementref is wrapper rond HTMLElement
+  @ViewChild('playField', { static: true }) playFieldRef!: ElementRef;
 
+  //variabelen
+  #scene!:THREE.Scene;
+  #renderer!: THREE.WebGLRenderer;
+  #camera!: THREE.PerspectiveCamera;
+  #grid!: Grid;
+
+  //scene definieren en grid object aanmaken + toevoegen
+  ngAfterViewInit(): void {
+    const container = this.playFieldRef.nativeElement;
+    const width = container.offsetWidth;
+    const height = container.offsetHeight;
+
+    this.#scene = new THREE.Scene();
+    this.#renderer = new THREE.WebGLRenderer();
+    this.#renderer.setSize(width, height);
+
+    //camera initialiseren, grid is 30, dus 60 genomen, width/height voor correct ratio, 0.1 omdat kleinste objecten 0.1 zijn en 1000 zodat ook bij groter grid en camera distance dit zichtbaar is
+    this.#camera = new THREE.PerspectiveCamera(60, width / height, 0.1, 1000);
+
+    container.appendChild(this.#renderer.domElement);
+    this.#scene.background = new THREE.Color(0x808080);
+
+    this.#camera.position.z = 50;
+    this.animate();
+
+    this.#grid = new Grid();
+    this.#grid.initialize();
+    this.#scene.add(this.#grid.mesh);
+    this.#scene.add(this.#grid.grid);
+    
+  }
+  //rendered de scene en grid
+  animate = () => {
+    requestAnimationFrame(this.animate);
+    this.#renderer.render(this.#scene, this.#camera);
+  }
 }
