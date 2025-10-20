7fee19cfdec5af06ce55b5307c691379f946d9ac
ANON <spunkburger3@gmail.com>
2025-10-20 20:48:16 +0200

    camera kan roteren

--- src/app/components/simulation-view/simulation-view.ts
+++ src/app/components/simulation-view/simulation-view.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-simulation-view',
+  standalone:true,
   imports: [],
   templateUrl: './simulation-view.html',
   styleUrl: './simulation-view.css'
 })
 export class SimulationView {
-
+  //haalt camera op uit DOM van simulation
+  @Input() camera!: THREE.PerspectiveCamera;
+  //variabelen meteen initialiseren
+  #radius = 30;
+  #azimuth = 0;
+  #elevation = 0;
+  #mouseDown = false;
+  #prevMouseX = 0;
+  #prevMouseY = 0;
+  //check of middelste muisknop ingedrukt is
+  onMouseDown(e: MouseEvent) { 
+    if (e.button === 1){
+      this.#mouseDown = true;
+    }
+  }
+  //check of middelste muisknop weer losgelaten is
+  onMouseUp(e: MouseEvent) { 
+    if (e.button === 1) {
+      this.#mouseDown = false;
+    }
+  }
+  //wat te doen als muis beweegt, azimuth is verandering in x waarde, elevation in y waarde, nadien met Math berekend of deze niet onder 0° gaat of boven 90°
+  onMouseMove(e: MouseEvent) { 
+    if(this.#mouseDown){
+      this.#azimuth -= ((e.clientX-this.#prevMouseX));
+      this.#elevation += ((e.clientY-this.#prevMouseY));
+      this.#elevation = Math.min(90, Math.max(0, this.#elevation));
+      this.updateCameraPosition();
+    }
+    this.#prevMouseX = e.clientX;
+    this.#prevMouseY = e.clientY; 
+  }
+  //mouse scroll voor in en uitzoomen, moet tussen z positie 20 en 100 blijven
+  onMouseWheel(e: WheelEvent) { 
+    const zoomSpeed = 0.1;
+    this.#radius += e.deltaY * zoomSpeed;
+    this.#radius = Math.max(20, Math.min(100, this.#radius));
+    this.updateCameraPosition();
+  }
+  //formules voor het correct roteren van de camera
+  updateCameraPosition() { 
+    this.camera.position.x = this.#radius * Math.sin(this.#azimuth * Math.PI/180) * Math.cos(this.#elevation * Math.PI/180);
+    this.camera.position.y = this.#radius * Math.sin(this.#elevation * Math.PI/180);
+    this.camera.position.z = this.#radius * Math.cos(this.#azimuth * Math.PI/180) * Math.cos(this.#elevation * Math.PI/180);
+    this.camera.lookAt(0, 0, 0);
+  }
 }
--- src/app/pages/simulation/simulation.ts
+++ src/app/pages/simulation/simulation.ts
@@ -2,10 +2,12 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import * as THREE from 'three';
 import { Grid } from '../../models/grid.model';
+import { SimulationView } from "../../components/simulation-view/simulation-view";
+import { StatsPanel } from "../../components/stats-panel/stats-panel";
 
 @Component({
   selector: 'app-simulation',
-  imports: [FormsModule],
+  imports: [FormsModule, SimulationView, StatsPanel],
   templateUrl: './simulation.html',
   styleUrl: './simulation.css'
 })
@@ -13,6 +15,7 @@ import { Grid } from '../../models/grid.model';
 export class Simulation implements AfterViewInit{
   //viewchild voor reference naar DOM element in plaats van values uit te lezen, static : true zodat playfield beschikbaar is oninit, elementref is wrapper rond HTMLElement
   @ViewChild('playField', { static: true }) playFieldRef!: ElementRef;
+  
 
   //variabelen
   #scene!:THREE.Scene;
@@ -43,6 +46,8 @@ export class Simulation implements AfterViewInit{
     this.#grid.initialize();
     this.#scene.add(this.#grid.mesh);
     this.#scene.add(this.#grid.grid);
+
+    
     
   }
   //rendered de scene en grid
@@ -50,4 +55,9 @@ export class Simulation implements AfterViewInit{
     requestAnimationFrame(this.animate);
     this.#renderer.render(this.#scene, this.#camera);
   }
+  //geeft de camera mee aan DOM element zodat view deze kan gebruiken
+  get fieldCamera(): THREE.PerspectiveCamera {
+    return this.#camera;
+  }
+
 }
