cb8b98d6376a9d100ba5a497ee636cd1b9deb5d4
ANON <spunkburger3@gmail.com>
2025-10-22 11:49:08 +0200

    nav afgewerkt, stats panel weergegeven en beetje css

--- src/app/components/stats-panel/stats-panel.ts
+++ src/app/components/stats-panel/stats-panel.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-stats-panel',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './stats-panel.css'
 })
 export class StatsPanel {
-
+  @Input() plantAmount!: number;
 }
