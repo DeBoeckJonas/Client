6cd1237bc5a599b1d222fa875d52b2ad65cdc16d
ANON <spunkburger3@gmail.com>
2025-10-27 10:34:27 +0100

    index redirect naar home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
 
 export const routes: Routes = [
+    // pathmatch full zodat niet alles naar home geredirect wordt (elk path start met '')
+    { path: '', redirectTo: 'home', pathMatch: 'full'},
     { path : 'simulation', component: Simulation},
     { path : 'home', component : Home},
     { path : 'about', component : About}
