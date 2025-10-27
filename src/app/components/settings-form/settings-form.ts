907103a1fc936d4f199bff20ce273ff62802ba22
ANON <spunkburger3@gmail.com>
2025-10-27 13:12:00 +0100

    settings form code in eigen component gezet

--- src/app/components/settings-form/settings-form.ts
+++ src/app/components/settings-form/settings-form.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { SimulationService } from '../../services/simulation';
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: 'app-settings-form',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './settings-form.html',
   styleUrl: './settings-form.css'
 })
 export class SettingsForm {
+  constructor(private simulationService : SimulationService){
+    
+  }
+  startForm = false;
+  showStartForm(){
+    this.startForm = true;
+    console.log(this.startForm)
+  }
 
+  start(plantAmount:string, herbAmount:string, carnAmount:string, herbSearchRange:string, carnSearchRange:string, herbTicks:string, carnTicks:string, plantTicks:string):void{
+    this.simulationService.herbSearchRangeInput = parseInt(herbSearchRange);
+    this.simulationService.carnSearchRangeInput = parseInt(carnSearchRange);
+    this.simulationService.startAmountPlantInput = parseInt(plantAmount);
+    this.simulationService.startAmountHerbInput = parseInt(herbAmount);
+    this.simulationService.startAmountCarnInput = parseInt(carnAmount);
+    this.simulationService.carnTickInput = parseInt(carnTicks);
+    this.simulationService.herbTickInput = parseInt(herbTicks);
+    this.simulationService.plantTickInput = parseInt(plantTicks);
+
+    this.simulationService.isStarted = true;
+    this.startForm = false;
+  }
 }
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,35 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Simulation } from '../simulation/simulation';
 import { SimulationService } from '../../services/simulation';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { SettingsForm } from "../../components/settings-form/settings-form";
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule, FormsModule],
+  imports: [RouterModule, FormsModule, SettingsForm],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
-  constructor(private simulationService : SimulationService){
-    
-  }
-  startForm = false;
-  showStartForm(){
-    this.startForm = true;
-    console.log(this.startForm)
-  }
-  start(plantAmount:string, herbAmount:string, carnAmount:string, herbSearchRange:string, carnSearchRange:string, herbTicks:string, carnTicks:string, plantTicks:string):void{
-    this.simulationService.herbSearchRangeInput = parseInt(herbSearchRange);
-    this.simulationService.carnSearchRangeInput = parseInt(carnSearchRange);
-    this.simulationService.startAmountPlantInput = parseInt(plantAmount);
-    this.simulationService.startAmountHerbInput = parseInt(herbAmount);
-    this.simulationService.startAmountCarnInput = parseInt(carnAmount);
-    this.simulationService.carnTickInput = parseInt(carnTicks);
-    this.simulationService.herbTickInput = parseInt(herbTicks);
-    this.simulationService.plantTickInput = parseInt(plantTicks);
+  @ViewChild(SettingsForm) settingsForm!:SettingsForm;
 
-    this.simulationService.isStarted = true;
-    this.startForm = false;
+  triggerSettingsForm() {
+    this.settingsForm.showStartForm();
   }
+  
 }
